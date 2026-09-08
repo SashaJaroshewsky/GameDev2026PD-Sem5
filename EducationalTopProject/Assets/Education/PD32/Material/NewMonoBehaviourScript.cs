a752733225b9171db0621c22baf6192e6b4954ec
JaroschPC <jaroschew@gmail.com>
2026-09-08 09:16:44 +0300

    Add pz1

---
+++ EducationalTopProject/Assets/Education/PD32/Material/NewMonoBehaviourScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Debug.Log("Update called");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate called");
+    }
+
+}
